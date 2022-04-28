4a7755ad1a08ea125a61093d83ee44d5c762a299
sgamza <sgamza@KRALAP01>
2022-04-28 16:29:51 +0200

    Dodaj pliki projektów.

---
+++ GamesShopBlazor/Shared/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamesShopBlazor.Shared
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public string ImageUrl { get; set; } = String.Empty;
+        public decimal Price { get; set; }
+    }
+}
